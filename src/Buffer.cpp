4988c1f05347093af11cf6fb3cdbaf5089c8557c
Tiago Sá Marques <samarques.tiago@gmail.com>
2020-08-03 10:01:54 +0100

    Linum: cleanups

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -11,23 +11,23 @@ Buffer::Buffer(QWidget *parent) :
   p.setColor(QPalette::Text, QColor::fromRgb(216, 222, 233));
   setPalette(p);
 
-  linum_area = new LineNumberArea(this);
+  linum_area = new Linum(this);
 
   QFont f("Source Code Pro");
   f.setStyleHint(QFont::Monospace);
   setFont(f);
 
-  connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
-  connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
+  connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLinumWidth(int)));
+  connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLinumArea(QRect, int)));
   connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
 
-  updateLineNumberAreaWidth(0);
+  updateLinumWidth(0);
   highlightCurrentLine();
 }
 
 
 int
-Buffer::lineNumberAreaWidth()
+Buffer::linumWidth()
 {
   int digits = 1;
   int max = qMax(1, blockCount());
@@ -44,15 +44,15 @@ Buffer::lineNumberAreaWidth()
 
 
 void
-Buffer::updateLineNumberAreaWidth(int)
+Buffer::updateLinumWidth(int)
 {
-  setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+  setViewportMargins(linumWidth(), 0, 0, 0);
 }
 
 
 
 void
-Buffer::updateLineNumberArea(const QRect &rect, int dy)
+Buffer::updateLinumArea(const QRect &rect, int dy)
 {
   if (dy)
     linum_area->scroll(0, dy);
@@ -60,7 +60,7 @@ Buffer::updateLineNumberArea(const QRect &rect, int dy)
     linum_area->update(0, rect.y(), linum_area->width(), rect.height());
 
   if (rect.contains(viewport()->rect()))
-    updateLineNumberAreaWidth(0);
+    updateLinumWidth(0);
 }
 
 
@@ -71,7 +71,7 @@ Buffer::resizeEvent(QResizeEvent *e)
   QPlainTextEdit::resizeEvent(e);
 
   QRect cr = contentsRect();
-  linum_area->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+  linum_area->setGeometry(QRect(cr.left(), cr.top(), linumWidth(), cr.height()));
 }
 
 
@@ -96,10 +96,8 @@ Buffer::highlightCurrentLine()
   setExtraSelections(extra_selection);
 }
 
-
-
 void
-Buffer::lineNumberAreaPaintEvent(QPaintEvent *event)
+Buffer::linumPaintEvent(QPaintEvent *event)
 {
   QPainter painter(linum_area);
   painter.fillRect(event->rect(), QColor::fromRgb(121, 125, 133));
--- src/Buffer.hpp
+++ src/Buffer.hpp
@@ -9,7 +9,7 @@ class QResizeEvent;
 class QSize;
 class QWidget;
 
-class LineNumberArea;
+class Linum;
 
 
 class Buffer : public QPlainTextEdit
@@ -17,54 +17,50 @@ class Buffer : public QPlainTextEdit
 Q_OBJECT
 
 public:
-    Buffer(QWidget *parent = nullptr);
+  Buffer(QWidget *parent = nullptr);
 
-    void
-    lineNumberAreaPaintEvent(QPaintEvent *event);
+  void linumPaintEvent(QPaintEvent *event);
 
-    int
-    lineNumberAreaWidth();
+  int linumWidth();
 
 protected:
-    void
-    resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
+  void
+  resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
 
 private slots:
-    void
-    updateLineNumberAreaWidth(int newBlockCount);
+  void updateLinumWidth(int newBlockCount);
 
-    void
-    highlightCurrentLine();
+  void
+  highlightCurrentLine();
 
-    void
-    updateLineNumberArea(const QRect &, int);
+  void updateLinumArea(const QRect& rect, int dy);
 
 private:
-    QWidget* linum_area;
+  QWidget* linum_area;
 };
 
 
-class LineNumberArea : public QWidget
+class Linum : public QWidget
 {
 public:
-    explicit LineNumberArea(Buffer*editor) :
-            QWidget(editor)
-    {
-        m_editor = editor;
-    }
+  explicit Linum(Buffer*editor) :
+      QWidget(editor)
+  {
+    m_editor = editor;
+  }
 
-    QSize
-    sizeHint() const Q_DECL_OVERRIDE
-    {
-        return QSize(m_editor->lineNumberAreaWidth(), 0);
-    }
+  QSize
+  sizeHint() const Q_DECL_OVERRIDE
+  {
+    return QSize(m_editor->linumWidth(), 0);
+  }
 
 protected:
-    void
-    paintEvent(QPaintEvent* event) Q_DECL_OVERRIDE
-    {
-        m_editor->lineNumberAreaPaintEvent(event);
-    }
+  void
+  paintEvent(QPaintEvent* event) Q_DECL_OVERRIDE
+  {
+    m_editor->linumPaintEvent(event);
+  }
 
 private:
   Buffer* m_editor;
